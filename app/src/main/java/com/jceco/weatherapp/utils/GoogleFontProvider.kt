70b76e13220f156ba07da565461510c64965c515
Joao Pedro Sehnem dos Santos <jpssantosos5874@gmail.com>
2025-06-06 15:47:50 -0300

    feat: created GoogleFontProvider to create custom font-families

--- app/src/main/java/com/jceco/weatherapp/utils/GoogleFontProvider.kt
+++ app/src/main/java/com/jceco/weatherapp/utils/GoogleFontProvider.kt
@@ -1,2 +1,18 @@
 package com.jceco.weatherapp.utils
 
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
+import com.jceco.weatherapp.R
+
+val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+val fontName = GoogleFont("Figtree")
+
+val figTreeFontFamily = FontFamily(
+    Font(googleFont = fontName, fontProvider = provider)
+)
