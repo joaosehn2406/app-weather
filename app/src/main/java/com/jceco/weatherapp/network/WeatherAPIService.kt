f66a84348f707b9c8bb5559882e92956cbd987c7
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-09 15:36:56 -0300

    refactor: implemented annotation for URL string

--- app/src/main/java/com/jceco/weatherapp/network/WeatherAPIService.kt
+++ app/src/main/java/com/jceco/weatherapp/network/WeatherAPIService.kt
@@ -5,6 +5,7 @@ import com.jceco.weatherapp.data.ForecastWeather
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Url
 
 private const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 
@@ -16,10 +17,10 @@ private val retrofit = Retrofit.Builder()
 interface WeatherApiService {
 
     @GET("weather?lat=26.927297526419704&lon=-49.25099473192019&appid=c3ca9fa126f4a03f93b7b2001c14a0c8")
-    suspend fun getCurrentWeather() : CurrentWeather
+    suspend fun getCurrentWeather(@Url endUrl: String) : CurrentWeather
 
     @GET("forecast?lat=26.927297526419704&lon=-49.25099473192019&appid=c3ca9fa126f4a03f93b7b2001c14a0c8")
-    suspend fun getForecastWeather() : ForecastWeather
+    suspend fun getForecastWeather(@Url endUrl: String) : ForecastWeather
 }
 
 object WeatherApi {
