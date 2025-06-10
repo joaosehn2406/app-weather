f6be08a4247e014ed6f0e474b3a1b51782cfd990
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-10 07:47:23 -0300

    refactor: created methods to Loading, Error and Success states

--- app/src/main/java/com/jceco/weatherapp/pages/WeatherHomeUiState.kt
+++ app/src/main/java/com/jceco/weatherapp/pages/WeatherHomeUiState.kt
@@ -1,2 +1,15 @@
 package com.jceco.weatherapp.pages
 
+import com.jceco.weatherapp.data.CurrentWeather
+import com.jceco.weatherapp.data.ForecastWeather
+
+data class Weather(
+    val currentWeather: CurrentWeather,
+    val forecastWeather: ForecastWeather
+)
+
+sealed interface WeatherHomeUiState {
+    data class Success(val weather: Weather) : WeatherHomeUiState
+    data object Error : WeatherHomeUiState
+    data object Loading : WeatherHomeUiState
+}
