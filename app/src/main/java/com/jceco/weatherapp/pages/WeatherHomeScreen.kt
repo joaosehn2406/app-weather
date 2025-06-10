17683e6db43e29e10065253bb49d6564eaa8870c
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-10 14:31:42 -0300

    feat: implemented cards for the temperature cards

--- app/src/main/java/com/jceco/weatherapp/pages/WeatherHomeScreen.kt
+++ app/src/main/java/com/jceco/weatherapp/pages/WeatherHomeScreen.kt
@@ -13,7 +13,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -34,6 +37,7 @@ import com.jceco.weatherapp.R
 import com.jceco.weatherapp.customui.AppBackground
 import com.jceco.weatherapp.data.CurrentWeather
 import com.jceco.weatherapp.data.ForecastWeather
+import com.jceco.weatherapp.data.ForecastWeather.ForecastItem
 import com.jceco.weatherapp.utils.DEGREE
 import com.jceco.weatherapp.utils.getFormattedDate
 import com.jceco.weatherapp.utils.getIconUrl
@@ -96,6 +100,7 @@ fun WeatherSection(
             currentWeather = weather.currentWeather,
             modifier = Modifier.weight(1f)
         )
+        ForecastWeatherSection(forecastItems = weather.forecastWeather.list)
     }
 }
 
@@ -139,7 +144,7 @@ fun CurrentWeatherSection(
                     .crossfade(true)
                     .build(),
                 contentDescription = "Ícone do clima",
-                modifier = Modifier.size(48.dp),
+                modifier = Modifier.size(48.dp)
             )
 
             Spacer(modifier = Modifier.width(12.dp))
@@ -156,7 +161,7 @@ fun CurrentWeatherSection(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier.fillMaxWidth()
         ) {
-            Column{
+            Column {
                 Text(
                     text = "Humidity ${currentWeather.main.humidity}%",
                     style = MaterialTheme.typography.titleMedium
@@ -171,11 +176,13 @@ fun CurrentWeatherSection(
                 )
             }
             Spacer(modifier = Modifier.width(10.dp))
-            Surface(modifier = Modifier
-                .width(2.dp)
-                .height(100.dp)) {}
+            Surface(
+                modifier = Modifier
+                    .width(2.dp)
+                    .height(100.dp)
+            ) {}
             Spacer(modifier = Modifier.width(10.dp))
-            Column{
+            Column {
                 Text(
                     text = "Sunrise ${getFormattedDate(currentWeather.sys.sunrise, "HH:mm")}",
                     style = MaterialTheme.typography.titleMedium
@@ -193,6 +200,53 @@ fun CurrentWeatherSection(
 }
 
 @Composable
-fun ForecastWeatherSection(modifier: Modifier = Modifier) {
-    
+fun ForecastWeatherSection(
+    forecastItems: List<ForecastWeather.ForecastItem>,
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(6.dp)
+    ) {
+        items(forecastItems.size) { index ->
+            ForecastWeatherItem(forecastItems[index])
+        }
+    }
+}
+
+@Composable
+fun ForecastWeatherItem(
+    item: ForecastWeather.ForecastItem,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        colors = CardDefaults.cardColors(containerColor = Color.Black.copy(alpha = 0.5f)),
+        modifier = modifier
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = modifier.padding(4.dp)
+        ) {
+            Text(
+                getFormattedDate(item.dt, pattern = "EEE"),
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                getFormattedDate(item.dt, pattern = "HH:mm"),
+                style = MaterialTheme.typography.titleMedium
+            )
+            Spacer(modifier = modifier.height(10.dp))
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(getIconUrl(item.weather[0].icon))
+                    .crossfade(true)
+                    .build(),
+                contentDescription = "Ícone do clima",
+                modifier = Modifier.size(48.dp)
+                    .padding(top = 4.dp, bottom = 4.dp)
+            )
+            Spacer(modifier = modifier.height(10.dp))
+            Text("${item.main.temp.toInt()}$DEGREE ", style = MaterialTheme.typography.titleMedium)
+        }
+    }
 }
