3c3e52bb0a9ab49fc7fe8d0e070f627d6eb39418
joaosehn2406 <jpssantosos5874@gmail.com>
2025-06-11 13:49:06 -0300

    feat: DI implemented in ConnectivityRepository

--- app/src/main/java/com/jceco/weatherapp/repository/ConnectivityRepository.kt
+++ app/src/main/java/com/jceco/weatherapp/repository/ConnectivityRepository.kt
@@ -7,12 +7,13 @@ import com.jceco.weatherapp.pages.ConnectivityState
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
 interface ConnectivityRepository {
     val connectivityState: StateFlow<ConnectivityState>
 }
 
-class DefaultConnectivityRepository(
+class DefaultConnectivityRepository @Inject constructor (
     private val connectivityManager: ConnectivityManager
 ) : ConnectivityRepository {
     private val _connectivityState =
